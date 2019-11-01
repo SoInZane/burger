8dce4601b4225760ed96d70695ed5acab829b91f
SoInZane <51385126+SoInZane@users.noreply.github.com>
2019-11-01 14:33:17 -0500

    added db files, installed express and added gitignore

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Green Chili Cheeseburger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sunrise Burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Triple Bypass Burger', FALSE);
